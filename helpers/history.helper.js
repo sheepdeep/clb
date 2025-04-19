5577ba9fc981b9e0e844170438188ab45a972731
SheepDeep <sheepdeep447@gmail.com>
2025-04-19 16:37:23 +0700

    fix vcb - mb

--- helpers/history.helper.js
+++ helpers/history.helper.js
@@ -382,11 +382,10 @@ exports.handleDesc = async (description) => {
         const desc = description.split('.');
         const newDesc = desc[3].split(' ');
 
-        console.log(newDesc);
 
         return {
             username: newDesc[0],
-            comment: newDesc[1].toUpperCase().replace(/\./g, '')
+            comment: newDesc[2].toUpperCase().replace(/\./g, '')
         };
     }
 
