cc47bbf7a95c0d364acfd7a584ef82c209606282
SheepDeep <sheepdeep447@gmail.com>
2025-04-30 19:37:13 +0700

    fix code

--- servers/taiXiuRong/service.js
+++ servers/taiXiuRong/service.js
@@ -318,8 +318,8 @@ exports.handleTurn = async () => {
 
                 if (comment === checkTurn.resultText) {
                     await historyModel.findByIdAndUpdate(history._id, {
-                        bonus: history.amount * dataSetting.banTaiXiu.ratio,
-                        description: `Bạn đã đặt cược <span class="code-num">${Intl.NumberFormat('en-US').format(history.amount)}</span> vnđ tại phòng chơi của <span class="code-num">TX Rồng</span>. (SB: ${Intl.NumberFormat('en-US').format(user.balance + history.amount)} -&gt; ${Intl.NumberFormat('en-US').format(user.balance)}) [THẮNG] [${checkTurn.xucxac1} - ${checkTurn.xucxac2} - ${checkTurn.xucxac3} = ${checkTurn.result}] (SB: ${Intl.NumberFormat('en-US').format(user.balance)} -&gt; ${Intl.NumberFormat('en-US').format(user.balance + (history.amount * dataSetting.banTaiXiu.ratio))})`,
+                        bonus: Math.floor(history.amount * dataSetting.banTaiXiu.ratio),
+                        description: `Bạn đã đặt cược <span class="code-num">${Intl.NumberFormat('en-US').format(history.amount)}</span> vnđ tại phòng chơi của <span class="code-num">TX Rồng</span>. (SB: ${Intl.NumberFormat('en-US').format(user.balance + history.amount)} -&gt; ${Intl.NumberFormat('en-US').format(user.balance)}) [THẮNG] [${checkTurn.xucxac1} - ${checkTurn.xucxac2} - ${checkTurn.xucxac3} = ${checkTurn.result}] (SB: ${Intl.NumberFormat('en-US').format(user.balance)} -&gt; ${Intl.NumberFormat('en-US').format(user.balance + Math.floor((history.amount * dataSetting.banTaiXiu.ratio)))})`,
                         result: 'win',
                         paid: 'sent'
                     })
